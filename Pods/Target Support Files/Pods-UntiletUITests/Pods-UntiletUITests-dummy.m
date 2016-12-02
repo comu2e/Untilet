a394d65a2ee18fa2e6ac01728276856fdb2b1aa1
comu2e <comu2e@gmail.com>
2016-12-02 20:39:10 +0900

    marge taki

---
+++ Pods/Target Support Files/Pods-UntiletUITests/Pods-UntiletUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UntiletUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UntiletUITests
+@end
--- Untilet/ViewController.swift
+++ Untilet/ViewController.swift
@@ -11,44 +11,69 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    //クラス変数
+//    var __data__:NSDictionary!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         LoadAPI()
     }
 
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBAction func LoadAPI_button(_ sender: AnyObject) {
+        LoadAPI()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     /*
-     API読み込み関数
+     メイン処理
+     呼び出し時：
+     ViewDidLoad時に一回
+     Loadボタンが押されるたびに一回ずつ呼び出される
+     流れ：
+     現在時刻の取得
+     現在時刻までのデータをAPIから呼び出し
+     JSONをパースする。
+     最新要素だけを得てそこからパーセント表示する
      */
-    func LoadAPI() {
+    func LoadAPI(){
+        let now = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"+"T"+"HH:mm:ss.000Z"
+        
         //HeaderはAPI key
         let headers = [
                         "X-M2X-KEY": "9875a0904ad6ff8c46a0ed47dd1730c3",
                        ]
         //APIURLはstreamに割り振られているAPI＿URLにする
-        let API_URL = "https://api-m2x.att.com/v2/devices/a0b52f8541b7e2a4a35617d42f6efe5d/streams/MQ4/sampling?type=nth&interval=2&start=2016-11-26T00:00:00.000Z&end=2016-11-28T00:00:00.000Z&limit=4&pretty"
+        
+        let streamID = "MQ4"
+        
+        let start = "2016-11-26T00:00:00.000Z"
+        let end = "2016-11-28T00:00:00.000Z"
+        let limit = 10
+        
+        let API_URL = "https://api-m2x.att.com/v2/devices/a0b52f8541b7e2a4a35617d42f6efe5d/streams/\(streamID)/sampling?type=nth&interval=1&start=\(start)&end=\(end)&limit=\(limit)&pretty"
         //start,endのパラメータは現時刻の時点にできるようにする
         Alamofire.request(API_URL,method:.get,headers: headers).responseJSON{response in
-            if let JSON = response.result.value{
-                print("=")
+            if let jsonDict = response.result.value as! NSDictionary!{
+            let values = jsonDict["values"] as! NSArray
+            
+            let leatest_element = values[0] as! NSDictionary
                 
-                print("JSON:\(JSON)")
+            let leatestTimeStamp = leatest_element["timestamp"] as! String
+            let leatestValue = leatest_element["value"] as! Int
+            
+            print(leatestTimeStamp)
+            print(leatestValue)
+            self.valueLabel.text = leatestValue.description
             }
-        }
         
-    }
-    
-    /*
-     JSON処理関数
-     */
-  
-    
+        }
 }
-
+}
